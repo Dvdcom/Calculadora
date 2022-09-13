9deca308a2489d640d642fa68e2b52cc47cea047
Davgutierrez <davgutierrez@teco.com.ar>
2022-09-13 20:48:31 -0300

    Avance de javascript

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 let ingreso = "";
 let num1;
 let num2;
+let numeros = new Array();
 let codOperacion;
 let resultado;
 
@@ -23,21 +24,28 @@ function asignar(x,y){
     if(y >= 48 && y <= 57 || y >= 96 && y <= 105 || y == 110 || y == 189 || y == 109){
         ingreso = ingreso + x; 
         pantalla.value = ingreso;
+
     }
     /* si presiona codigo de operacion */
     if(x == '+' || x == '*' || x == '/'){
-        if(num1 == ""){
-            num1 = parseint(pantalla.value);}
-            else if(num2 == "" && num1 !== ""){ 
-                num2 = parseint(pantalla.value);}
-            else{ingreso = ""}
+        numeros.push(ingreso);
+        ingreso = "";
         pantalla.value = x;
         codOperacion = x;
+        numeros.push(codOperacion);
     }
     /* si presiona el =  */
     if( y == 187 || y == 13){
-        console.log('num1 = ' + num1);
-        console.log('num2 = ' + num2);
+        numeros.push(ingreso);
+        /* recorrer el array */
+        for (i = 0; i < numeros.length; i++) {
+            if(!isNAN(numeros[i])){
+                console.log(numeros[i]);
+            }
+            /*https://www.freecodecamp.org/espanol/news/foreach-en-javascript-como-recorrer-un-arreglo-en-js/ */
+            /* https://www.neoguias.com/comprobar-valor-numero-javascript/ */
+        };
+
         switch (codOperacion){
             case '+':
                 resultado = suma(num1,num2);
