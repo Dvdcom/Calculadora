a86da88b76dd7562fbb2018c702808a21c55c5b9
David Gutierrez <gutierrezdvde@gmail.com>
2022-09-14 12:11:05 -0300

    ultimos apuntes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /* variables principales */
 let ingreso = "";
 let resultado = 0;
-let seDioResultado =false;
+let seDioResultado = false;
 let numeros = new Array();
 
 const pantalla = document.querySelector('#in-resultado');
@@ -11,25 +11,25 @@ document.addEventListener('keydown', (event) => {
     var codeValue = event.keyCode;
 
     console.log(codeValue + " " + keyValue);
-    if ( codeValue == 8 || codeValue >= 48 && codeValue <= 57 || codeValue >= 96 && codeValue <= 107 || codeValue >= 109 && codeValue <= 111 || codeValue == 13 || codeValue == 187 || codeValue == 190 ){
-        asignar(keyValue,codeValue);
+    if (codeValue == 8 || codeValue >= 48 && codeValue <= 57 || codeValue >= 96 && codeValue <= 107 || codeValue >= 109 && codeValue <= 111 || codeValue == 13 || codeValue == 187 || codeValue == 190) {
+        asignar(keyValue, codeValue);
     }
 
 }, false);
 
 /* Calculadora basada en ingreso de teclado */
-function asignar(x,y){
+function asignar(x, y) {
     /* si presiona un numero */
-    if(y >= 48 && y <= 57 || y >= 96 && y <= 105 || y == 110){
+    if (y >= 48 && y <= 57 || y >= 96 && y <= 105 || y == 110) {
         /* CONCATENO ENTRADA Y LA MUESTRO EN PANTALLA */
-        ingreso = ingreso + x; 
+        ingreso = ingreso + x;
         pantalla.value = ingreso;
     }
     /* si presiona codigo de operacion ( + - * / ) */
-    if(x == '+' || x == '-' || x == '*' || x == '/'){
-        
+    if (x == '+' || x == '-' || x == '*' || x == '/') {
+
         /* consulto si tengo o no un resultado anterior dado */
-        if (!seDioResultado){numeros.push(parseInt(pantalla.value));}
+        if (!seDioResultado) { numeros.push(parseInt(pantalla.value)); }
         /* RESETEO A NADA LA VARIABLE INGRESO */
         ingreso = "";
         /* MUESTRO EN PANTALLA LA OPERACION QUE QUIERO REALIZAR  */
@@ -40,7 +40,7 @@ function asignar(x,y){
     }
 
     /* si presiona el = o enter indicando que quiero un resultado */
-    if( y == 187 || y == 13){
+    if (y == 187 || y == 13) {
 
         /* GUARDO EL SEGUNDO NUMERO INGRESADO DENTRO DEL ARREGLO */
         numeros.push(parseInt(ingreso));
@@ -54,67 +54,67 @@ function asignar(x,y){
             operacion.push(numero);
 
             /* SOLO VA A ENTRAR EN ESTE IF CUANDO EL PRIMER NUMERO Y EL SEGUNDO NUMERO SE INGRESEN (ES DECIR PARA ESTE ENTONCES LAS 3 PRIMERAS POSICIONES SE LLENARON) */
-            if(operacion[0] > 0  && operacion[2] > 0){
+            if (operacion[0] > 0 && operacion[2] > 0) {
 
-            /* console.log de testeo */
-            /* console.log('num1 es = ' + operacion[0] + ' num2 es = ' + operacion[2] + ' codOperacion es = ' + operacion[1]); */
+                /* console.log de testeo */
+                /* console.log('num1 es = ' + operacion[0] + ' num2 es = ' + operacion[2] + ' codOperacion es = ' + operacion[1]); */
 
-            /* REALIZO LA OPERACION ENTREGANDO TODO LOS 3 DATOS */
-                switch (operacion[1]){
-            case '+':
-                resultado = suma(operacion[0],operacion[2]);
-                break;
-            case '-':
-                resultado = resta(operacion[0],operacion[2]);
-                break;
-            case '*':
-                resultado = multiplicacion(operacion[0],operacion[2]);
-                break;
-            case '/':
-                resultado = division(operacion[0],operacion[2]);
-                break;
+                /* REALIZO LA OPERACION ENTREGANDO TODO LOS 3 DATOS */ /* utilizar un arreglo para guardar funciones */
+                switch (operacion[1]) {
+                    case '+':
+                        resultado = suma(operacion[0], operacion[2]);
+                        break;
+                    case '-':
+                        resultado = resta(operacion[0], operacion[2]);
+                        break;
+                    case '*':
+                        resultado = multiplicacion(operacion[0], operacion[2]);
+                        break;
+                    case '/':
+                        resultado = division(operacion[0], operacion[2]);
+                        break;
                 }
 
-                if(numeros.length>3){
+                if (numeros.length > 3) {
                     operacion = [];
                     operacion.push(resultado)
-                    }
                 }
+            }
         });
         /* doy un resultado en pantalla y cambio el boolean a verdadero */
-            pantalla.value = resultado;
-            seDioResultado = true;
-        };
+        pantalla.value = resultado;
+        seDioResultado = true;
+    };
 
-        /* cuando se preciona la tecla BORRAR */
-        if(y == 8){
-            pantalla.value = "";
-            ingreso = "";
-            numeros = [];
-            resultado = "";
-            seDioResultado = false;
-        }
+    /* cuando se preciona la tecla BORRAR */
+    if (y == 8) {
+        pantalla.value = "";
+        ingreso = "";
+        numeros = [];
+        resultado = "";
+        seDioResultado = false;
+    }
 }
-    
+
 
 /* crear la operacion */
-function suma(num1,num2){
+function suma(num1, num2) {
     return num1 + num2;
 }
-function resta(num1,num2){
+function resta(num1, num2) {
     return num1 - num2;
 }
-function multiplicacion(num1,num2){
+function multiplicacion(num1, num2) {
     return num1 * num2;
 }
-function division(num1,num2){
+function division(num1, num2) {
     return num1 / num2;
 }
 
-function esImpar(x){
-    return (x%2==0) ? true : false;
+function esImpar(x) {
+    return (x % 2 == 0) ? true : false;
 }
 
-function simulaIngreso(tecla,codigo){
-    asignar(tecla,codigo);
+function simulaIngreso(tecla, codigo) {
+    asignar(tecla, codigo);
 }
